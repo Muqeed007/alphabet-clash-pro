0b9ff5e2ab226eca45734517ef54a588c060a7be
Muqeed007 <ahmedalmuqeed2002@gmail.com>
2024-02-18 14:29:16 +0600

    keyboard functionality done

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,7 +7,22 @@
 //     playgroundSection.classList.remove('hidden');   
 // }
 
+function continueGame(){
+    // generating randem alphabet
+    const alphabet = getRandomAlphabet();
+
+    // set randomly generated alphabet on screen
+    const currentAlphabetNumber = document.getElementById('current-alphabet');
+    currentAlphabetNumber.innerText = alphabet;
+
+    // set background color
+    setBackGroundColorById(alphabet);
+    // removeBackGroundColorById(alphabet);
+
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
--- Scripts/uitillity.js
+++ Scripts/uitillity.js
@@ -6,3 +6,26 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackGroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-500');
+}
+
+// function removeBackGroundColorById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('bg-orange-500')
+// }
+
+function getRandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    // get a random index between 0 - 25 (a - z)
+    const randomAlphabet = Math.random()*25;
+    const index = Math.round(randomAlphabet);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
