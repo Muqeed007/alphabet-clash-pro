28baff6665a48dbd2df0afe35bf5a1aba7d143b3
Muqeed007 <ahmedalmuqeed2002@gmail.com>
2024-02-18 13:07:10 +0600

    UI Section Done

---
+++ Scripts/script.js
@@ -0,0 +1,13 @@
+// function play(){
+//     // step-1 --> hide the home screen
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // step-2 --> show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');   
+// }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
